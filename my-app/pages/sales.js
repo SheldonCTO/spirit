2d9f8ffad36b6d580b757cccc276c9e56cf1c725
SheldonCto <c.cto1981@gmail.com>
2024-12-01 13:22:27 -0500

    sale update

---
+++ my-app/pages/sales.js
@@ -0,0 +1,11 @@
+import { Card, Form, Alert, Button } from "react-bootstrap";
+import { useState, useEffect } from 'react';
+import NewProduct from './newProduct';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+export default function Sales(){
+	const [sale, setSale] = useState([])
+
+
+}
