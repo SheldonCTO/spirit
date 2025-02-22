13742e3b19544c4b42fbe4d37e3f1aa375f470a7
cc-kcc <115552496+cc-kcc@users.noreply.github.com>
2025-02-22 16:59:55 -0500

    update orders table

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: process.env.DB_DEV_HOST,
-    port: Number(process.env.DB_DEV_PORT),
-    user: process.env.DB_DEV_USER,
-    password: process.env.DB_DEV_PASSWORD,
-    database: process.env.DB_DEV_NAME,
+    host: process.env.DB_PROD_HOST,
+    port: Number(process.env.DB_PROD_PORT),
+    user: process.env.DB_PROD_USER,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
